5e839e869b826b2bfb54538db1d5f67bdaa3ef31
Zhehao Wang <wangzhehao410305@gmail.com>
2014-08-22 17:31:34 -0700

    sync-based-discovery: Data and interest logic implementation

--- test-conference-discovery-sync.cpp
+++ test-conference-discovery-sync.cpp
@@ -1,28 +1,69 @@
 #include "test-conference-discovery-sync.h"
+#include <sys/time.h>
+#include <openssl/ssl.h>
 
 using namespace ndn;
 using namespace std;
 
 using namespace conference_discovery;
 
-void SyncBasedDiscovery::onData
+void 
+SyncBasedDiscovery::onData
   (const ptr_lib::shared_ptr<const Interest>& interest,
    const ptr_lib::shared_ptr<Data>& data)
 {
+  cout << "Data " << data->getName().toUri() << " received." << endl;
   
+  string content;
+  for (size_t i = 0; i < data->getContent().size(); ++i)
+    content += (*data->getContent())[i];
+  
+  vector<std::string> objects = SyncBasedDiscovery::stringToObjects(content);
+  
+  cout << "Data content contained " << content << endl;
+  
+  // Finding vector differences by using existing function, 
+  // which requires both vectors to be sorted.
+  std::sort(objects.begin(), objects.end());
+  std::sort(objects_.begin(), objects_.end());
+
+  std::vector<std::string> setDifferences;
+
+  std::set_symmetric_difference(
+    objects.begin(),
+    objects.end(),
+    objects_.begin(),
+    objects_.end(),
+    std::back_inserter(setDifferences));
+  
+  onReceivedSyncData_(setDifferences, false);
 }
 
-void SyncBasedDiscovery::onTimeout
+void 
+SyncBasedDiscovery::onTimeout
   (const ptr_lib::shared_ptr<const Interest>& interest)
 {
-  cout << "Sync interest times out." << endl;
+  cout << "Sync interest times out, re-expressing sync interest." << endl;
+  
+  // Do I or do I not want to updateHash here? I don't think that I do.
+  Name interestName(broadcastPrefix_);
+  interestName.append(currentDigest_);
+  
+  Interest newInterest(interestName);
+  newInterest.setInterestLifetimeMilliseconds(defaultInterestLifetime_);
+    
+  face_.expressInterest
+    (newInterest, bind(&SyncBasedDiscovery::onData, this, _1, _2), 
+     bind(&SyncBasedDiscovery::onTimeout, this, _1));
 }
 
-void SyncBasedDiscovery::onInterest
+void 
+SyncBasedDiscovery::onInterest
   (const ptr_lib::shared_ptr<const Name>& prefix,
    const ptr_lib::shared_ptr<const Interest>& interest, Transport& transport,
    uint64_t registerPrefixId)
 {
+  cout << "Interest " << interest->getName().toUri() << " received. Not getting answered by MCS" << endl;
   // memoryContentCache should be able to satisfy interests marked with "ANSWER_NO_CONTENT_STORE",
   // Will it cause potential problems?
   string syncDigest = interest->getName().get
@@ -30,21 +71,138 @@ void SyncBasedDiscovery::onInterest
     
   if (syncDigest != currentDigest_) {
     // the syncDigest differs from the local knowledge, reply with local knowledge
+    // We don't have digestLog implemented in this, so incremental replying is not considered now
+    
     // It could potentially cause one name corresponds with different data in different locations,
     // Same as publishing two conferences simultaneously: such errors should be correctible
     // later steps
+    Data data(interest->getName());
+    std::string content = objectsToString();
+    data.setContent((const uint8_t *)&content[0], content.size());
+    data.getMetaInfo().setTimestampMilliseconds(time(NULL) * 1000.0);
+    data.getMetaInfo().setFreshnessPeriod(defaultDataFreshnessPeriod_);
     
+    keyChain_.sign(data, certificateName_);
+    Blob encodedData = data.wireEncode();
+
+    transport.send(*encodedData);
   }
   else if (syncDigest != newComerDigest_) {
     // Store this steady-state (outstanding) interest in application PIT, unless neither the sender
-    // and receiver knows anything
-    
-    
+    // nor receiver knows anything
+    pendingInterestTable_.push_back(ptr_lib::shared_ptr<PendingInterest>
+      (new PendingInterest(interest, transport)));
   }
 }
-    
-void SyncBasedDiscovery::onRegisterFailed
+
+void 
+SyncBasedDiscovery::onRegisterFailed
   (const ptr_lib::shared_ptr<const Name>& prefix)
 {
-  
-}    
+  cout << "Prefix registration for name " << prefix->toUri() << " failed." << endl;
+}
+
+void
+SyncBasedDiscovery::updateHash()
+{
+// TODO: hash implementation
+/*
+  SHA256_CTX sha256;
+
+  SHA256_Init(&sha256);
+  uint8_t number[4];
+  int32ToLittleEndian(seqno_session_, number);
+  SHA256_Update(&sha256, number, sizeof(number));
+  int32ToLittleEndian(seqno_seq_, number);
+  SHA256_Update(&sha256, number, sizeof(number));
+  uint8_t digest_seq[SHA256_DIGEST_LENGTH];
+  SHA256_Final(digest_seq, &sha256);
+
+  SHA256_Init(&sha256);
+  SHA256_Update(&sha256, &dataPrefix_[0], dataPrefix_.size());
+  uint8_t digest_name[SHA256_DIGEST_LENGTH];
+  SHA256_Final(digest_name, &sha256);
+
+  SHA256_Init(&sha256);
+  SHA256_Update(&sha256, digest_name, sizeof(digest_name));
+  SHA256_Update(&sha256, digest_seq, sizeof(digest_seq));
+  uint8_t digest_node[SHA256_DIGEST_LENGTH];
+  SHA256_Final(digest_node, &sha256);
+  digest_ = toHex(digest_node, sizeof(digest_node));
+*/
+}
+
+void
+SyncBasedDiscovery::publishObject(std::string name)
+{
+  // addObject sorts the objects array
+  if (addObject(name)) {
+    Name dataName = Name(broadcastPrefix_).append(currentDigest_);
+    Data data(dataName);
+    
+    std::string content = objectsToString();
+    data.setContent((const uint8_t *)&content[0], content.size());
+    data.getMetaInfo().setTimestampMilliseconds(time(NULL) * 1000.0);
+    data.getMetaInfo().setFreshnessPeriod(defaultDataFreshnessPeriod_);
+    
+    keyChain_.sign(data, certificateName_);
+    contentCacheAdd(data);
+    
+    updateHash();
+    
+    Name interestName = Name(broadcastPrefix_).append(currentDigest_);
+    Interest interest(interestName);
+    interest.setInterestLifetimeMilliseconds(defaultInterestLifetime_);
+    
+    face_.expressInterest
+      (interest, bind(&SyncBasedDiscovery::onData, this, _1, _2), 
+       bind(&SyncBasedDiscovery::onTimeout, this, _1));
+  }
+  else {
+    cout << "Object already exists." << endl;
+  }
+}
+
+void
+SyncBasedDiscovery::contentCacheAdd(const Data& data)
+{
+  contentCache_.add(data);
+
+  // Remove timed-out interests and check if the data packet matches any pending
+  // interest.
+  // Go backwards through the list so we can erase entries.
+  MillisecondsSince1970 nowMilliseconds = ndn_getNowMilliseconds();
+  for (int i = (int)pendingInterestTable_.size() - 1; i >= 0; --i) {
+    if (pendingInterestTable_[i]->isTimedOut(nowMilliseconds)) {
+      pendingInterestTable_.erase(pendingInterestTable_.begin() + i);
+      continue;
+    }
+
+    if (pendingInterestTable_[i]->getInterest()->matchesName(data.getName())) {
+      try {
+        // Send to the same transport from the original call to onInterest.
+        // wireEncode returns the cached encoding if available.
+        pendingInterestTable_[i]->getTransport().send
+          (*data.wireEncode());
+      }
+      catch (std::exception& e) {
+      }
+
+      // The pending interest is satisfied, so remove it.
+      pendingInterestTable_.erase(pendingInterestTable_.begin() + i);
+    }
+  }
+}
+
+SyncBasedDiscovery::PendingInterest::PendingInterest
+  (const ptr_lib::shared_ptr<const Interest>& interest, Transport& transport)
+  : interest_(interest), transport_(transport)
+{
+  // Set up timeoutTime_.
+  if (interest_->getInterestLifetimeMilliseconds() >= 0.0)
+    timeoutTimeMilliseconds_ = ndn_getNowMilliseconds() +
+      interest_->getInterestLifetimeMilliseconds();
+  else
+    // No timeout.
+    timeoutTimeMilliseconds_ = -1.0;
+}
--- test-conference-discovery-sync.h
+++ test-conference-discovery-sync.h
@@ -19,7 +19,8 @@
 #include <ndn-cpp/encoding/binary-xml-wire-format.hpp>
 #include <ndn-cpp/util/memory-content-cache.hpp>
 
-#endif
+#include <boost/algorithm/string.hpp>
+#include <sys/time.h>
 
 using namespace std;
 using namespace ndn::func_lib;
@@ -27,6 +28,15 @@ using namespace ndn;
 
 namespace conference_discovery
 {
+  static MillisecondsSince1970 
+  ndn_getNowMilliseconds()
+  {
+    struct timeval t;
+    // Note: configure.ac requires gettimeofday.
+    gettimeofday(&t, 0);
+    return t.tv_sec * 1000.0 + t.tv_usec / 1000.0;
+  }
+  
   /**
    * This class, upon instantiation, will register a broadcast prefix and 
    * will handle all the logics going on in that broadcast namespace,
@@ -49,15 +59,16 @@ namespace conference_discovery
     // Does initiation part in constructor implementation do a copy, or merely a reference?
     // Question: does this initiation part call the constructor, or do merely an equal: 
     // seems to be the former, which means, it copies, unless the variable being initialized is a reference member
+    // Double check: the thing with cpp initialization sequence in constructor
     
     // This class does not own the face. Their relationships are aggregation
     SyncBasedDiscovery
       (Name broadcastPrefix, const OnReceivedSyncData& onReceivedSyncData, 
        Face& face, KeyChain& keyChain, Name certificateName)
      : broadcastPrefix_(broadcastPrefix), onReceivedSyncData_(onReceivedSyncData), 
-       newComerDigest_("00"), face_(face), keyChain_(keyChain), 
-       certificateName_(certificateName), contentCache_(&face), 
-       currentDigest_(newComerDigest_)
+       face_(face), keyChain_(keyChain), certificateName_(certificateName), 
+       contentCache_(&face), newComerDigest_("00"), currentDigest_(newComerDigest_),
+       defaultDataFreshnessPeriod_(4000), defaultInterestLifetime_(2500)
     {
       // Storing it in contentCache, the idea is that a set of strings maps to a digest
       contentCache_.registerPrefix
@@ -79,6 +90,11 @@ namespace conference_discovery
       
     };
     
+    /**
+     * onData sorts both the object(string) array received, 
+     * and the string array belonging to this object.
+     * I think I need a lock for objects_ member...
+     */
     void onData
       (const ptr_lib::shared_ptr<const Interest>& interest,
        const ptr_lib::shared_ptr<Data>& data);
@@ -94,7 +110,33 @@ namespace conference_discovery
     void onRegisterFailed
       (const ptr_lib::shared_ptr<const Name>& prefix);
     
+    /**
+     * contentCacheAdd copied from ChronoSync2013 implementation
+     * Double check its logic
+     *
+	 * Add the data packet to the contentCache_. Remove timed-out entries
+	 * from pendingInterestTable_. If the data packet satisfies any pending
+	 * interest, then send the data packet to the pending interest's transport
+	 * and remove from the pendingInterestTable_.
+	 * @param data
+	 */
+    void contentCacheAdd(const Data& data);
+    
+    /**
+     * Called when startPublishing, note that this function itself does 
+     * add the published name to objects_. The caller should not call addObject to do it agin.
+     * @ {string} name: the full (prefix+conferenceName).toUri()
+     */
+    void publishObject(std::string name);
+    
+    /**
+     * Updates the currentDigest_ according to the list of objects
+     */
+    void updateHash();
+    
 	const std::string newComerDigest_;
+	const Milliseconds defaultDataFreshnessPeriod_;
+	const Milliseconds defaultInterestLifetime_;
     
     /**
      * These functions should be replaced, once we replace objects with something more
@@ -102,14 +144,23 @@ namespace conference_discovery
      */
     std::vector<std::string> getObjects() { return objects_; };
     
+    // addObject does not necessarily call updateHash
     int addObject(std::string object) {
-      objects_.push_back(object);
-      // Using default comparison '<' here
-      std::sort(objects_.begin(), objects_.end()); 
-      // Update the currentDigest_ 
-      return 1;
+      std::vector<std::string>::iterator item = std::find(objects_.begin(), objects_.end(), object);
+      if (item == objects_.end() && object != "") {
+        objects_.push_back(object);
+        // Using default comparison '<' here
+        std::sort(objects_.begin(), objects_.end()); 
+        // Update the currentDigest_ 
+        return 1;
+      }
+      else {
+        // The to be added string exists or is empty
+        return 0;
+      }
     };
     
+    // removeObject does not necessarily call updateHash
     int removeObject(std::string object) {
       std::vector<std::string>::iterator item = std::find(objects_.begin(), objects_.end(), object);
       if (item != objects_.end()) {
@@ -123,6 +174,81 @@ namespace conference_discovery
       }
     };
     
+    // To and from string method using \n as splitter
+    std::string objectsToString() {
+      std::string result;
+      for(std::vector<std::string>::iterator it = objects_.begin(); it != objects_.end(); ++it) {
+        result += *it;
+        result += "\n";
+      }
+      return result;
+    }
+    
+    static std::vector<std::string> stringToObjects(std::string str) {
+      std::vector<std::string> objects;
+      boost::split(objects, str, boost::is_any_of("\n"));
+      // According to the insertion process above, we may have an empty string at the end
+      if (objects.back() == "") {
+        objects.pop_back();
+      }
+      return objects;
+    }
+    
+    /**
+     * Should-be-replaced methods end
+     */
+    
+    /**
+	 * PendingInterest class copied from ChronoSync2013 cpp implementation
+	 *
+	 * A PendingInterest holds an interest which onInterest received but could
+	 * not satisfy. When we add a new data packet to the contentCache_, we will
+	 * also check if it satisfies a pending interest.
+	 */
+	class PendingInterest {
+	public:
+	  /**
+	   * Create a new PendingInterest and set the timeoutTime_ based on the current time and the interest lifetime.
+	   * @param interest A shared_ptr for the interest.
+	   * @param transport The transport from the onInterest callback. If the
+	   * interest is satisfied later by a new data packet, we will send the data
+	   * packet to the transport.
+	   */
+	  PendingInterest
+		(const ptr_lib::shared_ptr<const Interest>& interest,
+		 Transport& transport);
+
+	  /**
+	   * Return the interest given to the constructor.
+	   */
+	  const ptr_lib::shared_ptr<const Interest>&
+	  getInterest() { return interest_; }
+
+	  /**
+	   * Return the transport given to the constructor.
+	   */
+	  Transport&
+	  getTransport() { return transport_; }
+
+	  /**
+	   * Check if this interest is timed out.
+	   * @param nowMilliseconds The current time in milliseconds from ndn_getNowMilliseconds.
+	   * @return true if this interest timed out, otherwise false.
+	   */
+	  bool
+	  isTimedOut(MillisecondsSince1970 nowMilliseconds)
+	  {
+		return timeoutTimeMilliseconds_ >= 0.0 && nowMilliseconds >= timeoutTimeMilliseconds_;
+	  }
+
+	private:
+	  ptr_lib::shared_ptr<const Interest> interest_;
+	  Transport& transport_;
+	  MillisecondsSince1970 timeoutTimeMilliseconds_; /**< The time when the
+		* interest times out in milliseconds according to ndn_getNowMilliseconds,
+		* or -1 for no timeout. */
+	};
+	
   private:
     Name broadcastPrefix_;
     Name certificateName_;
@@ -134,10 +260,15 @@ namespace conference_discovery
     
     KeyChain& keyChain_;
     
-    // This serves as the rootDigest in ChronoSync
+    // This serves as the rootDigest in ChronoSync.
     std::string currentDigest_;
     // This serves as the list of objects to be synchronized, for now.
-    // Could be replaced with a Protobuf class or a class later
+    // Could be replaced with a Protobuf class or a class later.
     std::vector<std::string> objects_;
+    
+    // PendingInterestTable for holding outstanding interests.
+    std::vector<ptr_lib::shared_ptr<PendingInterest> > pendingInterestTable_;
   };
 }
+
+#endif
